2edcb4c33b6b7d976179885c7f75c1fbe13cb6f7
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-07-16 12:26:36 +0530

    modified : added feature fo empwage calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,12 @@
 #! /bin/bash -x
 ispreentfull=1
-ispresenthalf=2
-empratehr
-echo "Welcome to employeewage problem"
+emprateperhr=20
+empcheck=$((RANDOM%2))
+if [ $empcheck -eq $ispresentfull ]
+then
+	emphrs=8
+else
+	emphrs=0
+fi
+
+salary=$(($emphrs * $emprateperhr))
